1a1ff15c33c0a84726d229365fcfd094019e1da5
JosiasRodrigues <120478571+JosiasRodrigues@users.noreply.github.com>
2023-09-16 17:11:14 -0300

    refiz todo o codigo

--- src/src/script.js
+++ src/src/script.js
@@ -1,16 +1,11 @@
-const toggLeMenu = () => {
-    const burgerMenu = document.querySelector(".menu-icon");
+const toggleMenu = () => {
+    const burgerMenu = document.querySelector(".menu_icon");
     const src = burgerMenu.getAttribute('src');
-    const iconName = src === 'burger-menu.svg' ?
-    'close.svg'
-    :
-    'burger-menu.svg';
+    const iconName = src.includes('burger-menu.svg') ? 'close.svg' : 'burger-menu.svg';
 
     burgerMenu.setAttribute('src', iconName);
 
     const navigation = document.querySelector('.navigation');
 
-    navigation.classList.toggle(
-        'navigation--mobile'
-    );
+    navigation.classList.toggle('navigation--mobile');
 };
